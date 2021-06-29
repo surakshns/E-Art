42dcaf4aea0b4432649f2d1af378a0d00d8b2b34
Suraksh eMedicHT <suraksh.emedicht@gmail.com>
2021-06-29 09:45:28 +0530

    redux corrected

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ export const listProduct=()=>async(dispatch)=>{
     try{
         dispatch({type: PRODUCT_LIST_REQUEST})
 
-        const { data } = await axios.get('/api/products')
+        const { data } = await axios.get('/api/products/')
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
