70b4d58b816937c8fa416a0b0ba9b1aa6f1d5e12
Srinidhi Gangavathi <srinidhigangavathi21@gmail.com>
2021-07-26 14:10:47 +0530

    admin_page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,42 +1,55 @@
 import express from 'express'
 const router = express.Router()
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+//import asyncHandler from 'express-async-handler'
+//import Product from '../models/productModel.js'
 
 import {
     getProducts,
     getProductById,
+    deleteProduct,
+    createProduct,
+    updateProduct,
+    createProductReview,
+    getTopProducts,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
-// @desc    Fetch all products
-// @route   GET/api/products
-// @access  Public 
-router.get(
-    '/',
-    asyncHandler(async (req, res) => {
-        const products = await Product.find({})
-        res.json(products)
-    })
-)
 
-// @desc Fetch single product
-// @route  GET/api/products/:id
-// @access Public
-router.get(
-    '/:id',
-    asyncHandler(async (req, res) => {
-        const product = await Product.findById(req.params.id)
+// // @desc    Fetch all products
+// // @route   GET/api/products
+// // @access  Public 
+// router.get(
+//     '/',
+//     asyncHandler(async (req, res) => {
+//         const products = await Product.find({})
+//         res.json(products)
+//     })
+// )
 
-        if (product) {
-            res.json(product)
-        } else {
-            res.status(404)
-            throw new Error('Product not found')
-        }
-    })
-)
+// // @desc Fetch single product
+// // @route  GET/api/products/:id
+// // @access Public
+// router.get(
+//     '/:id',
+//     asyncHandler(async (req, res) => {
+//         const product = await Product.findById(req.params.id)
+
+//         if (product) {
+//             res.json(product)
+//         } else {
+//             res.status(404)
+//             throw new Error('Product not found')
+//         }
+//     })
+// )
 
 export default router
